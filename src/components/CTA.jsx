13c005056242927da559d2cc26e490483af634d4
Lateef Kassim <lateefconcept@gmail.com>
2025-04-06 13:26:17 +0100

    added cv

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import CV from '../assets/lateefcv.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="#" className='btn'> Download CV</a>
+        <a href={CV} className='btn'> Download CV</a>
         <a href="#contact" className='btn btn-primary'> Let's Talk</a>
     </div>
   )
